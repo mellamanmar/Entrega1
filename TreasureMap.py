25d52763f8681ac85aac6307b8b2a992eff9318f
mellamanmar <marimelissamc@gmail.com>
2023-04-26 16:22:25 -0500

    Terminando de agregar el código de Treasure Map

--- TreasureMap.py
+++ TreasureMap.py
@@ -1,1 +1,21 @@
 # Elabora un código sobre una búsqueda del tesoro
+
+print ("Bienvenido a la isla. Tu misión será encontrar el tesoro")
+decision1= input ("Elige, izquierda o derecha. ") .lower()
+
+if decision1 == "izquierda":
+    decision2= input ("¿Nadar o esperar?. ") .lower()
+    
+    if decision2 == "esperar":
+        decision3= input ("Escoge una puerta: Rojo, Azul ó Amarillo. ") .lower()
+        if decision3 == "amarillo":
+            print ("¡¡HAS GANADO!!")
+        elif decision3 == "rojo":
+            print ("Eres quemado. GAME OVER")
+        else:
+            print ("Devorado por bestias. GAME OVER")
+
+    else:
+        print ("Atacado por una tribu. GAME OVER")
+else:
+    print ("Caíste en agujero. GAME OVER")
